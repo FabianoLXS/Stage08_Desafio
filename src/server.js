c7edab85b6149246eeb472844fc99a37844736fa
FabianoLXS <fab_luiz@msn.com>
2023-07-24 18:10:15 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
-const express = require("express")
-const app = express()
+require("express-async-errors")
+
 const migrationsRun = require("./database/sqlite/migrations")
+
+const AppError = require("./utils/error")
+
+const express = require("express")
+
+const routes = require("./routes")
+
+const app = express()
+app.use(express.json())
+app.use(routes)
+
+migrationsRun()
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    })
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  })
+})
 
 const PORT = 3333
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
-
-app.get("/", (request, response) => {
-  response.send("Hello World")
-})
